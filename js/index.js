5715c03565216dc0eb602ed8318766e14f16be89
Lorena Benesey <lorena.benesey@gmail.com>
2022-11-12 10:20:01 +0100

    done

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@
 
 function updateSubtotal(product) {
   const price = parseFloat(product.querySelector('.price span').innerText);
-  console.log(price);
   const quantity = product.querySelector('.quantity input').value;
-  console.log(quantity);
   const result = price * quantity;
   const subtotal = (product.querySelector('.subtotal span').innerText = result);
 
@@ -12,23 +10,22 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  /*const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);*/
 
   const productArray = document.querySelectorAll('.product');
   const newArr = [...productArray];
   let total = newArr.reduce((acc, curr) => {
-    return acc + updateSubtotal(product);
+    return acc + updateSubtotal(curr);
   }, 0);
- document.querySelector('#total-value span').innerText = total;
+  document.querySelector('#total-value span').innerText = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-event.currentTarget.parentNode.parentNode.remove();
+  event.currentTarget.parentNode.parentNode.remove();
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
 
 // ITERATION 5
@@ -37,9 +34,12 @@ function createProduct() {
   //... your code goes here
 }
 
-/*window.addEventListener('load', () => {
+window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeButton = document.getElementsByClassName('btn btn-remove');
+  removeButton.addEventListener('click', removeProduct());
+
   //... your code goes here
-});*/
+});
